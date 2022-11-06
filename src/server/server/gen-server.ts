e4757581f595d7bee5b22b90fe5f8eab81de954e
Hayden O'Neill <hayden.oneill9@gmail.com>
2022-11-07 07:45:30 +1300

    Add start of server

--- src/server/server/gen-server.ts
+++ src/server/server/gen-server.ts
@@ -11,6 +11,22 @@ const defaultOptions: Required<ServerOptions> = {
   port: 8000,
 };
 
+interface StateItem {
+  _type: string;
+}
+
+interface StateRequest {
+  state: StateItem[];
+}
+
+const isValidItem = (item: any): item is StateItem =>
+  typeof item === 'object' && typeof item._type === 'string';
+
+const isValidBody = (body: any): body is StateRequest =>
+  typeof body === 'object' &&
+  body.state instanceof Array &&
+  body.state.every(isValidItem);
+
 export class GenServer {
   private options: Required<ServerOptions>;
 
@@ -28,6 +44,26 @@ export class GenServer {
     const port = this.options.port;
     app.listen(port, () => {
       console.log('Running on port ' + port);
+    });
+    app.post('/v1/state', async (req, res) => {
+      const body = req.body;
+      if (!isValidBody(body)) {
+        res.status(400);
+        res.send({
+          message: 'Invalid state body.',
+        });
+        return;
+      }
+
+      const types = body.state.map((s) => s._type);
+      res.send({
+        validResources: types.filter((t) =>
+          this.resources.some((r) => r.constructor.name === t)
+        ),
+        invalidResources: types.filter((t) =>
+          this.resources.every((r) => r.constructor.name !== t)
+        ),
+      });
     });
   }
 }
