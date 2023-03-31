3deb8e8f54246c4b8fd7ecce4e91e1f2156a8073
Hayden O'Neill <hayden.oneill9@gmail.com>
2023-03-31 17:48:51 +1300

    Add start of web socket connections

--- packages/server/src/server/gen-server.ts
+++ packages/server/src/server/gen-server.ts
@@ -9,7 +9,7 @@ import {
   Generator,
   ErasedDesiredState,
 } from '@haydenon/gen-core';
-import { isStateRequest } from './models/state-requests';
+import { isStateRequest, StateRequest } from './models/state-requests';
 import {
   DesiredStateMapper,
   getContextForDesiredState,
@@ -62,11 +62,14 @@ export class GenServer {
 
     const wsServer = new ws.Server({ noServer: true });
     wsServer.on('connection', (socket) => {
-      socket.on('message', (message) => console.log(message));
+      socket.on('message', (message) => {
+        this.handleWebSocketMessage(message.toString('utf8'), (value) =>
+          socket.send(value)
+        );
+      });
     });
 
     server.on('upgrade', (request, socket, head) => {
-      console.log(request.url);
       if (request.url === '/v1') {
         wsServer.handleUpgrade(request, socket, head, (socket) => {
           wsServer.emit('connection', socket, request);
@@ -83,47 +86,136 @@ export class GenServer {
 
     app.post('/v1/state', async (req, res) => {
       const body = req.body;
-      if (!isStateRequest(body)) {
-        res.status(400);
+
+      const resp = await this.handleStateCreation(body);
+      if (resp.success) {
+        res.send(resp.body);
+      } else {
+        const status = resp.errorType === ErrorType.ClientError ? 400 : 500;
+        res.status(status);
         res.send({
-          errors: [{ message: 'Invalid state body.' }],
-        });
-        return;
-      }
-
-      const context = getContextForDesiredState(this.resources, body.state);
-      if (context instanceof Array) {
-        this.sendErrors(res, context);
-        return;
-      }
-      const mappedState = body.state.map((s) => this.mapper(s, context));
-      const errors = mappedState.filter((s) => s instanceof Array) as Error[][];
-      if (errors.length > 0) {
-        this.sendErrors(res, errors.flat());
-        return;
-      }
-
-      const desired = mappedState as ErasedDesiredState[];
-
-      const generator = Generator.create(desired);
-      try {
-        const { createdState, desiredState } = await generator.generateState();
-        const response: StateCreateResponse = {
-          createdState: createdState.map(mapResourceInstanceToResponse),
-          desiredState: desiredState.map(mapDesiredStateToResponse),
-        };
-        res.send(response);
-      } catch (err) {
-        const error = err as GenerationResultError;
-        res.status(400);
-        res.send({
-          errors: [
-            error.errors?.map((e) => ({
-              messages: e.message,
-            })) ?? { message: error.message },
-          ],
+          errors: resp.errors.map((err) => ({ message: err })),
         });
       }
     });
   }
+
+  private handleWebSocketMessage(body: string, send: (data: string) => void) {
+    try {
+      const message = JSON.parse(body) as ClientMessage;
+      if (!message || !message.type || typeof message.type !== 'string') {
+        send('Error: invalid body');
+        return;
+      }
+
+      switch (message.type) {
+        case ClientMessageType.CreateState:
+          this.handleCreateMessage(message.body, send);
+          break;
+      }
+    } catch {
+      send('Error: invalid body');
+    }
+  }
+
+  private async handleCreateMessage(
+    body: StateRequest,
+    send: (data: string) => void
+  ) {
+    const result = await this.handleStateCreation(body);
+    if (result.success) {
+      send(JSON.stringify(result.body));
+    } else {
+      send(JSON.stringify(result.errors));
+    }
+  }
+
+  private async handleStateCreation(
+    body: any
+  ): Promise<Response<StateCreateResponse>> {
+    if (!isStateRequest(body)) {
+      return createClientError('Invalid state body.');
+    }
+
+    const context = getContextForDesiredState(this.resources, body.state);
+    if (context instanceof Array) {
+      return createClientError(context.map((err) => err.message));
+    }
+    const mappedState = body.state.map((s) => this.mapper(s, context));
+    const errors = mappedState.filter((s) => s instanceof Array) as Error[][];
+    if (errors.length > 0) {
+      return createClientError(errors.flat().map((err) => err.message));
+    }
+
+    const desired = mappedState as ErasedDesiredState[];
+
+    const generator = Generator.create(desired);
+    try {
+      const { createdState, desiredState } = await generator.generateState();
+      const response: StateCreateResponse = {
+        createdState: createdState.map(mapResourceInstanceToResponse),
+        desiredState: desiredState.map(mapDesiredStateToResponse),
+      };
+      return createSuccess(response);
+    } catch (err) {
+      const error = err as GenerationResultError;
+
+      return createServerError(
+        error.errors?.map((e) => e.message) ?? [error.message]
+      );
+    }
+  }
 }
+
+enum ClientMessageType {
+  CreateState = 'CreateState',
+}
+
+interface ClientMessageBase {
+  type: ClientMessageType;
+}
+
+interface CreateStateMessage extends ClientMessageBase {
+  type: ClientMessageType.CreateState;
+  body: StateRequest;
+}
+
+type ClientMessage = CreateStateMessage;
+
+function createSuccess<T>(body: T): SuccessResponse<T> {
+  return { success: true, body };
+}
+
+function createClientError(message: string | string[]): ErrorResponse {
+  return {
+    success: false,
+    errorType: ErrorType.ClientError,
+    errors: typeof message === 'string' ? [message] : message,
+  };
+}
+
+function createServerError(message: string | string[]): ErrorResponse {
+  return {
+    success: false,
+    errorType: ErrorType.ServerError,
+    errors: typeof message === 'string' ? [message] : message,
+  };
+}
+
+enum ErrorType {
+  ClientError,
+  ServerError,
+}
+
+interface ErrorResponse {
+  success: false;
+  errorType: ErrorType;
+  errors: string[];
+}
+
+interface SuccessResponse<T> {
+  success: true;
+  body: T;
+}
+
+type Response<T> = SuccessResponse<T> | ErrorResponse;
